2bcb258757a717d419eb9d0d307385edb107ccc3
Michael Bishop <michael.bishop@iohk.io>
2019-10-25 05:56:45 -0300

    commit all changes that where done on the deployer

--- thing/default.nix
+++ thing/default.nix
@@ -3,14 +3,38 @@ let
   jlib = import "${sources.jormungandr-nix}/lib.nix";
   inherit (jlib) lib;
   pkgs = import sources.nixpkgs {};
+  ada = n: n * 1000000; # lovelace
   inputConfig = __toFile "input.json" (__toJSON {
-    stakePoolBalances = [ 1 2 3 ];
-    stakePoolCount = 1500;
+    stakePoolBalances =
+      (__genList (_: ada 1000000) 50) ++
+      (__genList (_: ada 35000) 1450);
+    stakePoolCount = 2;
+    #stakePoolCount = 10;
+    inputBlockchainConfig = blockchainConfig;
   });
+
+  blockchainConfig = {
+    bft_slots_ratio = 0;
+    block0_consensus = "genesis_praos";
+    block0_date = __currentTime;
+    consensus_genesis_praos_active_slot_coeff = 0.1;
+    consensus_leader_ids = [];
+    discrimination = "test";
+    epoch_stability_depth = 10;
+    kes_update_speed = 86400;
+    linear_fees = {
+      certificate = 10000;
+      coefficient = 50;
+      constant = 1000;
+    };
+    max_number_of_transactions_per_block = 255;
+    slot_duration = 2;
+    slots_per_epoch = 900;
+  };
 in lib.fix (self: {
   jcli = jlib.pkgs.jormungandr-cli;
   ghc = pkgs.haskellPackages.ghcWithPackages (ps: with ps; [ aeson turtle ]);
-  thing = pkgs.runCommand "thing" { buildInputs = [ self.ghc self.jcli ]; src = ./.; inherit inputConfig; } ''
+  thing = pkgs.runCommand "thing" { buildInputs = [ self.ghc self.jcli pkgs.haskellPackages.ghcid ]; src = ./.; inherit inputConfig; } ''
     unpackPhase
     cd $sourceRoot
     mkdir -pv $out/bin/
@@ -20,6 +44,7 @@ in lib.fix (self: {
     set -e
     export PATH=${lib.makeBinPath [ self.jcli ]}:$PATH
     ${self.thing}/bin/thing ${inputConfig}
-    cat genesis.initial.yaml | jq '{ initial: ., genesis_config_things: 42 }' > genesis.yaml
+    jcli --version
+    jcli genesis encode < genesis.yaml > block-0.bin
   '';
 })
