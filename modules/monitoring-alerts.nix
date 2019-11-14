66914835fa1e7ba87157a49c143ee1bdee60c818
John Lotoski <john.lotoski@iohk.io>
2019-11-13 20:42:51 -0500

    Updates jormungandr monitoring alerts

--- modules/monitoring-alerts.nix
+++ modules/monitoring-alerts.nix
@@ -8,32 +8,28 @@
         severity = "page";
       };
       annotations = {
-        summary = "{{$labels.alias}}: Jormungandr block divergence detected for more than 5 minutes";
-        description = "{{$labels.alias}}: Jormungandr block divergence detected for more than 5 minutes";
+        summary = "{{$labels.alias}}: Jormungandr block divergence detected";
+        description = "{{$labels.alias}}: Jormungandr block divergence detected for more than 30 minutes and 20 blocks";
       };
     }
     {
       alert = "jormungandr_blockheight_unchanged";
       expr = "rate(jormungandr_lastBlockHeight[5m]) == 0";
       for = "30m";
-      #for = "10m";
       labels.severity = "page";
       annotations = {
-       summary = "{{$labels.alias}} Jormungandr blockheight unchanged for >=30mins";
+        summary = "{{$labels.alias}} Jormungandr blockheight unchanged";
         description = "{{$labels.alias}} Jormungandr blockheight unchanged for >=30mins.";
-        #summary = "{{$labels.alias}} Jormungandr blockheight unchanged for >=10mins";
-        #description = "{{$labels.alias}} Jormungandr blockheight unchanged for >=10mins.";
       };
     }
     {
       alert = "jormungandr_faucetFunds_monitor";
       expr = ''(jormungandr_address_funds{alias=~"faucet.*"} / 1e6) < 5000000'';
-      #expr = "jormungandr_faucetFunds < 10000000000000";
       for = "5m";
       labels.severity = "page";
       annotations = {
+        summary = "{{$labels.alias}} Jormungandr faucet wallet balance low alert";
         description = "{{$labels.alias}} Jormungandr faucet wallet balance is low (< 5M ADA)";
-        #description = "{{$labels.alias}} Jormungandr faucet wallet balance is low (< 10M ADA)";
       };
     }
     {
@@ -42,6 +38,7 @@
       for = "5m";
       labels.severity = "page";
       annotations = {
+        summary = "{{$labels.alias}} Prometheus WAL corruption alert";
         description = "{{$labels.alias}} Prometheus WAL corruption total is changing or a no data condition has been detected";
       };
     }
