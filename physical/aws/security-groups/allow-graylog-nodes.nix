2634c6cacb60a8be2d6b58da9b12a2af74191034
Michael Fellinger <michael.fellinger@iohk.io>
2019-12-02 18:09:02 +0100

    fix security groups for graylog

--- physical/aws/security-groups/allow-graylog-nodes.nix
+++ physical/aws/security-groups/allow-graylog-nodes.nix
@@ -1,16 +1,18 @@
 { lib, region, accessKeyId, nodes, ... }:
 let
-  inherit (lib) foldl' recursiveUpdate mapAttrs' nameValuePair flip;
-in flip mapAttrs' nodes (name: node:
-  nameValuePair "allow-graylog-nodes-${region}" ({resources, ...}: {
+  inherit (lib) nameValuePair mapAttrsToList;
+in {
+  "allow-graylog-nodes-${region}" = ({resources, ...}: {
     inherit region accessKeyId;
     _file = ./allow-graylog-nodes.nix;
     description = "Allow Graylog nodes ${region}";
-    rules = [{
-      protocol = "tcp"; # all
-      fromPort = 5044;
-      toPort = 5044;
-      sourceIp = resources.elasticIPs."${name}-ip";
-    }];
-  })
-)
+    rules = mapAttrsToList (subname: _:
+      {
+        protocol = "tcp"; # all
+        fromPort = 5044;
+        toPort = 5044;
+        sourceIp = resources.elasticIPs."${subname}-ip";
+      }
+    ) nodes;
+  });
+}
