c5de0068f70693950ce81109bad02ce2cf177c2b
Michael Fellinger <michael.fellinger@iohk.io>
2019-12-11 14:23:43 +0100

    serve registry.zip from explorer

--- roles/jormungandr-explorer.nix
+++ roles/jormungandr-explorer.nix
@@ -1,9 +1,28 @@
-{ lib, config, resources, name, ... }:
+{ pkgs, lib, config, resources, name, ... }:
 let inherit (config.node) fqdn;
   inherit (import ../nix {}) jormungandr-master;
   enableSSL = config.deployment.targetEnv != "libvirtd";
   protocol = if enableSSL then "https" else "http";
-  inherit (import ../globals.nix) domain;
+  inherit (import ../globals.nix) domain environment;
+
+  registryFiles = __filterSource (path: type:
+    (__match ''.*ed25519_(.*)\.(sig|json)$'' path) != null
+  ) ../secrets/pools;
+  registryZip = pkgs.runCommandNoCC "registry.zip" {
+    buildInputs = with pkgs; [ zip ];
+    inherit registryFiles;
+  } ''
+    dir=${environment}-testnet-stakepool-registry-master/registry
+    mkdir -p $dir
+    cp $registryFiles/* $dir
+    zip -r -9 $out $dir
+  '';
+
+  registryRoot = pkgs.runCommand "stakepool-registry" {} ''
+    mkdir -p $out/stakepool-registry
+    cp ${registryFiles}/* $out/stakepool-registry/
+    cp ${registryZip} $out/stakepool-registry/registry.zip
+  '';
 in {
   imports = [ ./jormungandr-relay.nix ];
 
@@ -19,6 +38,7 @@ in {
     inherit enableSSL;
     jormungandrApi = "${protocol}://${fqdn}/explorer/graphql";
   };
+
   services.nginx = {
     enable = true;
     recommendedTlsSettings = true;
@@ -30,7 +50,7 @@ in {
     commonHttpConfig = ''
       map $http_origin $origin_allowed {
         default 0;
-        https://shelley-testnet-explorer-nightly.netlify.com 1;
+        https://shelley-testnet-explorer-${environment}.netlify.com 1;
       }
 
       map $origin_allowed $origin {
@@ -38,6 +58,14 @@ in {
         1 $http_origin;
       }
     '';
+
+    virtualHosts.${fqdn}.locations."/stakepool-registry" = {
+      extraConfig = ''
+        allow all;
+        autoindex on;
+        root ${registryRoot};
+      '';
+    };
   };
 
   #services.jormungandr.rest.cors.allowedOrigins = [ "https://shelley-testnet-explorer-qa.netlify.com" ];
